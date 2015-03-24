d11ae3c25653aea8aecf56a7ce4822cb4477c821
Rodion Aznaurov <radik_az@mail.ru>
2015-03-24 12:13:54 +0200

    Body BG ready.

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@ var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
   gulp.src('assets/scss/**/*.scss')
-    .pipe(sass())
+    .pipe(sass({ errLogToConsole: true }))
     .pipe(gulp.dest('assets/css'))
 });
 
 gulp.task('watch', function() {
-  gulp.watch('assets/scss/**/*.scss', ['default']);
+  gulp.watch('assets/scss/**/*.scss', ['sass']);
 });
 
 gulp.task('default', ['sass', 'watch']);

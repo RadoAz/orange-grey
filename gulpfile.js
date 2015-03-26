a7a7b37e03ed92b1267e15f6fd588e2eeba8614f
Rodion Aznaurov <radik_az@mail.ru>
2015-03-26 10:31:49 +0200

    Commit to download.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-
-gulp.task('sass', function() {
-  gulp.src('assets/scss/**/*.scss')
-    .pipe(sass({ errLogToConsole: true }))
-    .pipe(gulp.dest('assets/css'))
+var path = 'assets/scss/**/*.scss';
+gulp.task('sass', function () {
+  gulp.src(path)
+    .pipe(sass())
+    .pipe(gulp.dest('./assets/css'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('assets/scss/**/*.scss', ['sass']);
+  gulp.watch(path, ['sass']);
 });
 
 gulp.task('default', ['sass', 'watch']);
+
+
